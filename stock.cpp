810d7f3e57bcf3f1429a3bbe109ab8030b400882
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-05 17:29:06 +0700

    public class sbtack

--- stock.cpp
+++ stock.cpp
@@ -17,4 +17,13 @@ public :
 class Stack
 {
 private :
+   Node *top; // Pointer to the top node of  the stack
+
+public :
+   Stack()
+   {
+    top * NULL; // Initialize the stack with a null top
+   }
+
+   //
 }
