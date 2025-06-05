89a47bae767cd5477cc5f71458ab7e0ac3eaacd0
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-05 18:04:17 +0700

    cout enter your choice

--- stock.cpp
+++ stock.cpp
@@ -89,7 +89,8 @@ int main()
         cout << "2. Pop\n";
         cout << "3. Peek\n";
         cout << "4. Exit\n";
-        
+        cout << "Enter your choice: ";
+      
     }
 
     return 0;
